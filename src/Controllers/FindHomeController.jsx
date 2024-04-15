92b06063b68b8809b6c9ef5e0296281bd2f2ea80
FuturaCreativa <admin@futuracreativa.com>
2024-04-15 14:04:24 -0700

    Cambios
    Find home

--- src/Controllers/FindHomeController.jsx
+++ src/Controllers/FindHomeController.jsx
@@ -6,59 +6,60 @@ import LikeBotton from "../Assets/Img/likebotton.png"
 
 const FindHomeController = ({ arrayCard }) => {
   return (
-    <div className=" mx-auto bg-[#002867] flex flex-col justify-center items-center">
+    <div className=" mx-auto bg-[#002867] flex flex-col justify-center items-center ">
       <h2 className="text-4xl font-bold text-center text-white py-20 mx-1">
         Explore Our Communities
       </h2>
-      <div className="flex flex-col lg:flex-row items-start justify-center py-20 sm:mx-auto w-full">
+      <div className="flex flex-col lg:flex-row items-start justify-center py-16 sm:mx-auto w-full">
         {/* Cards a la izquierda */}
-        <div className="max-w-8xl lg:w-1/2 xl:w-1/7 flex flex-col items-center justify-center lg:mx-0 mx-3">
+        <div className="max-w-8xl lg:w-1/2 xl:w-1/7 flex flex-col items-center justify-center mx-auto lg:mx-8">
         {arrayCard.map((item, index) => (
-            <div key={index} className="rounded overflow-hidden shadow-lg bg-white mt-10 border border-white grid grid-cols-1 lg:grid-cols-3 gap-4">
-              {/* Image */}
+          <div key={index} className="rounded overflow-hidden shadow-lg bg-white mt-10 border-8 border-white grid grid-cols-1 lg:grid-cols-2 " style={{ borderRadius: "5px" }}>
+          {/* Image */}
+          <img
+            className="col-span-1 lg:col-span-1 w-full h-92 "
+            src={ImageCard}
+            alt="Property"
+          />
+          {/* Content */}
+          <div className="col-span-1 lg:col-span-1 px-6 py-4 flex flex-col">
+            <div className=" text-3xl font-semibold mb-2 text-black flex lg:flex items-center">
+              Shady Canyon
               <img
-                className="col-span-1 lg:col-span-1 w-full"
-                src={ImageCard}
+                className="ml-auto mb-1 cursor-pointer"
+                src={LikeBotton}
                 alt="Property"
               />
-              {/* Content */}
-              <div className="col-span-1 lg:col-span-2 px-6 py-4 flex flex-col">
-                <div className="font-bold text-xl mb-2 text-black flex lg:flex items-center">
-                  Shady Canyon
-                  <img
-                    className="ml-auto mb-1 cursor-pointer"
-                    src={LikeBotton}
-                    alt="Property"
-                  />
-                </div>
-                <p className="text-gray-700 text-base mt-2 lg:mt-0 lg:ml-0">
-                  ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                </p>
-                {/* Details */}
-                <div className="py-7 mt-12 lg:px-4">
-                  <div className="grid grid-cols-3 gap-x-4">
-                    <div className="text-black items-center flex flex-col">
-                      <p className="text-xs">Average Value</p>
-                      <p className="text-xs font-black">$7.5M</p>
-                    </div>
-                    <div className="text-black items-center flex flex-col">
-                      <p className="text-xs">Price Range</p>
-                      <p className="text-xs font-black">$6.1 M - 24.5M</p>
-                    </div>
-                    <div className="text-black items-center flex flex-col">
-                      <p className="text-xs">Price Range</p>
-                      <p className="text-xs font-black">$6.1 M - 24.5M</p>
-                    </div>
-                  </div>
-                </div>
+            </div>
+            <p className="text-[#808080] text-base font-Poppins mt-2 lg:mt-0 lg:ml-0">
+              ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+            {/* Details */}
+            <div className="py-7 mt-8 lg:px-4">  
+            <div className="grid grid-cols-2 gap-x-4 lg:grid-cols-3 gap-y-4">  
+            <div className="items-center flex flex-col">
+                <p className="font-Poppins text-xs text-[#808080] ">Average Value</p>
+                <p className="text-xs mr-0 sm:mr-12 font-black">$7.5M</p>
+              </div>
+              <div className="items-center flex flex-col">
+                <p className="font-Poppins mr-0 sm:mr-10 text-xs text-[#808080]">List Price</p>
+                <p className="text-xs font-black">$6.1 M - 24.5M</p>
+              </div>
+              <div className="items-center flex flex-col">
+                <p className="font-Poppins text-xs  text-[#808080]">Home for Sale</p>
+                <p className="text-xs mr-0 sm:mr-6 font-black">8 Listings</p>
               </div>
             </div>
+          </div>
+          </div>
+        </div>
+
           ))}
         </div>
         {/* Mapa a la derecha */}
-        <div className=" w-full lg:w-1/2 xl:w-2/1  px-8 py-4 mx-auto">
-          <div className="relative w-full h-[400px] lg:h-[1300px]">
-            <iframe className="absolute top-0 left-0 w-full h-full" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.73732861526!2d-74.0059418!3d40.7127847!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM40zMDA2JzEwLjAiTiA3NMKwMjUnMzcuNyJX!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <div className=" w-full lg:w-1/2 xl:w-2/1  px-8 py-4 mx-auto flex lg:justify-end" >
+          <div className="relative w-full h-[400px] lg:h-[1300px] " >
+            <iframe className="absolute top-0 left-0 w-full h-full" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12080.73732861526!2d-74.0059418!3d40.7127847!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM40zMDA2JzEwLjAiTiA3NMKwMjUnMzcuNyJX!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" style={{ borderRadius: "5px" }}></iframe>
           </div>
         </div>
       </div>
--- src/Views/FindHome.jsx
+++ src/Views/FindHome.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Footer from "../Components/Common/Fotter";
-import Home from "../Assets/Img/Vector.png";
 import FindHomeController from "../Controllers/FindHomeController";
 import Ring from "../Assets/Img/anillop.png";
-import { RiHome2Line } from 'react-icons/ri';
-import { RiMoneyDollarBoxLine } from "react-icons/ri";
-import { AiOutlineSearch } from 'react-icons/ai';
-import { RiSendPlaneLine } from "react-icons/ri";
 import item from "../Assets/Img/item.png";
 import Styles from "../Assets/Styles/SeccionHeader.module.css"
+import range from "../Assets/Img/Icon.png";
+import home from "../Assets/Img/Vector.png"
+import addres from "../Assets/Img/Icon2.png"
 
 
 
@@ -20,16 +18,16 @@ const FindHome = () => {
 
   return (
     <>
-      <div className=" relative">
-        <section className="bg-[url(./Assets/Img/HeaderFindHome.png)]  h-screen bg-cover  flex flex-col items-center justify-center opacity-40 ">
-          <div className="text-center text-white mb-8">
-            <h1 className="text-4xl font-bold">Find a Home</h1>
-
-          </div>
-          <div className="w-full text-white relative">
-            <form className="flex flex-col sm:flex-row items-center justify-center">
-              <div className="bg-[#14253E] opacity-65 rounded-md flex items-center outline-none select select-bordered w-5/6 max-w-sm pl-4 py-4 sm:py-8 rounded-md text-center focus:outline-none mb-4 sm:mb-0 sm:rounded-s-4xl border-r-2 sm:border-white">
-                <RiHome2Line className="text-gray-400 ml-12 w-32 h-8" />
+      <div className=" relative  ">
+      <section className="bg-[url(./Assets/Img/HeaderFindHome.png)] h-screen bg-cover  flex flex-col items-center justify-center bg-black ">
+    <div className="absolute bg-black opacity-5 top-0 left-0 w-full h-full "></div>  
+    <div className="text-center  mb-8">
+      <h1 className="text-4xl text-white font-bold">Find a Home</h1>
+    </div>
+          <div className="w-full text-white relative ">
+            <form className="flex flex-col sm:flex-row items-center justify-center ">
+              <div className="bg-[#14253E] opacity-65 rounded-md flex items-center outline-none select select-bordered w-56 max-w-sm pl-4 py-4 sm:py-9 rounded-md text-center focus:outline-none mb-4 sm:mb-0 sm:rounded-s-4xl border-r-2 sm:border-white pr-4">
+                <img src={home} className="text-gray-400 ml-12 w-4 h-4" />
                 <select className="outline-none select select-bordered w-5/6 max-w-sm  rounded-md text-center focus:outline-none bg-transparent opacity-65">
                   <option disabled selected>
                     Type of house
@@ -38,27 +36,31 @@ const FindHome = () => {
                   <option>Greedo</option>
                 </select>
               </div>
-              <div className="bg-[#14253E] opacity-65 rounded-md flex items-center outline-none select select-bordered w-5/6 max-w-sm pl-4 py-4 sm:py-8 rounded-md text-center focus:outline-none mb-4 sm:mb-0 border-r-2 sm:border-white">
-                <RiMoneyDollarBoxLine className="text-gray-400 ml-12 w-32 h-8" />
-                <select className="outline-none select select-bordered w-5/6 max-w-sm  rounded-md text-center focus:outline-none bg-transparent opacity-65">
-                  <option disabled selected>
-                    Type of house
-                  </option>
-                  <option>Han Solo</option>
-                  <option>Greedo</option>
-                </select>
+              <div className="bg-[#14253E] opacity-65 rounded-md flex items-center outline-none select select-bordered w-48 max-w-sm pl-8 py-4 sm:py-9 rounded-md text-center focus:outline-none mb-4 sm:mb-0 border-r-2 sm:border-white pr-10">
+                <div className="flex">  
+                <img src={range} className="text-gray-400 ml-8 w-4 h-4 mt-1" />  
+                <select className="outline-none select select-bordered w-full max-w-sm flex-grow rounded-md text-center focus:outline-none bg-transparent opacity-65">
+                    <option disabled selected>
+                      Range
+                    </option>
+                    <option>Han Solo</option>
+                    <option>Greedo</option>
+                  </select>
+                </div>
               </div>
 
-              <div className="bg-[#14253E] opacity-65 rounded-md flex items-center outline-none select select-bordered w-5/6 max-w-sm pl-4 py-4 sm:py-8  sm:rounded-r-4xl text-center focus:outline-none mb-4 sm:mb-0">
-                <RiSendPlaneLine className="text-gray-400 w-32 h-8" />
-                <input type="search" name="search" placeholder="City, Neighborhood, ZIP, Address" className="bg-[#14253E] opacity-65 w-5/6 sm:w-full sm:rounded-r-4xl focus:outline-none mb-4 sm:mb-0 rounded-full mt-4 sm:mt-0" />
+              <div class="relative w-full max-w-lg flex items-center">
+                <div className="relative w-full max-w-lg flex items-center bg-[#14253E] opacity-65  pl-4 py-4 sm:py-9 rounded-r-4xl focus:outline-none">
+                <img src={addres} className="text-gray-400 ml-8 w-4 h-4 mr-4" /> 
+                <input type="search" name="search" placeholder="City, Neighborhood, ZIP, Address" class="bg-[#14253E] w-full focus:outline-none"/>
+                </div>
+                <button type="submit" class="absolute right-0 top-0 px-16 py-4 sm:py-9 rounded-4xl bg-[#0A182E] text-white" >
+                  Search
+                </button>
               </div>
-              <button type="submit" className="w-5/6 sm:w-auto px-16 py-4 sm:py-8 rounded-l-none sm:rounded-l-4xl rounded-4xl sm:rounded-4xl bg-[#0A182E] text-white">
-                Search
-              </button>
             </form>
           </div>
-          ]
+          
         </section>
         <img
           className="absolute  z-10 right-0  -mt-44 invisible md:visible "
